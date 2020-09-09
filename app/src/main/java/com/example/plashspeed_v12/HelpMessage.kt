2af0f1e8ae2a0b6afe6d7a2be6855ee3b8110748
darrenyip96 <51224609+darrenyip96@users.noreply.github.com>
2020-09-09 18:16:37 +0800

    new activity and tested

--- app/src/main/java/com/example/plashspeed_v12/HelpMessage.kt
+++ app/src/main/java/com/example/plashspeed_v12/HelpMessage.kt
@@ -1,29 +1,32 @@
 package com.example.plashspeed_v12
 
+//import android.support.v7.app.AlertDialog;
 import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import kotlinx.android.synthetic.main.activity_profile.*
-import kotlinx.android.synthetic.main.help_page.*
-//import android.support.v7.app.AlertDialog;
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.help_page.*
 
 class HelpMessage : AppCompatActivity() {
 
-    fun onSubmit(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.help_page)
         val builder = AlertDialog.Builder(this)
+        val intent = Intent(this, MainActivity::class.java)
 
         btnSubmit.setOnClickListener{
             builder.setTitle("Confirmation!")
             builder.setMessage("Your details had been saved, we will contact you soon.")
-            //builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = closeContextMenu()))
+            builder.setCancelable(true)
+            builder.setPositiveButton("OK", DialogInterface.OnClickListener{ dialogInterface: DialogInterface, i: Int ->
+                startActivity(intent)
+            })
+
             builder.show()
+            //val intent = Intent(this, MainActivity::class.java)
+            //startActivity(intent)
 
         }
     }
