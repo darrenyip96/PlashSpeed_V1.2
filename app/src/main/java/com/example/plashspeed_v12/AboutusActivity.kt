570c983bb971e8fcbe3e3843918339fed1d84194
Moogilan <davidmugilan@gmail.com>
2020-09-10 12:42:13 +0800

    MugiPayment

---
+++ app/src/main/java/com/example/plashspeed_v12/AboutusActivity.kt
@@ -0,0 +1,12 @@
+package com.example.plashspeed_v12
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AboutusActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.aboutus)
+    }
+}
